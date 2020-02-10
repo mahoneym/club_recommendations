213fe5a6cfeb1f38af567e3af7a640940b0a764c
maggiemahoney <mahoneym6@xavier.edu>
2020-02-10 17:20:28 -0500

    added some to recommender's init

--- recommender.py
+++ recommender.py
@@ -5,16 +5,20 @@ class Recommender:
     users                   # a dictionary of the students
     clubs                   # a dictionary of the created clubs
 
-    def __init__():
+    def __init__(self):
         # read in the data for users
+        users = []                      # initiate to an empty array
+        readUserData()
         # read in the data for clubs
+        clubs = []                      # initiate to an empty array
+        readClubData()
         return 0
 
     def addUser():
         # call __init__ of the User class
         return 0
 
-    def readClubData():
+    def readClubData(self):
         return 0
 
     def readUserData():
