a8de99dde221c2098659c92f33aac32dd128eb44
maggiemahoney <mahoneym6@xavier.edu>
2020-02-21 17:05:09 -0500

    created a larger data set- so far it seems to be working with it.. I may add some more clubs to it

--- tester.py
+++ tester.py
@@ -1,43 +1,75 @@
 import recommender
 
-recommendObject = recommender.Recommender()
-
-def addClubs():
+def addMiniClubs():
     recommendObject.addClub('Computer Science Club', 'Academic', 1)
     recommendObject.addClub('Pep Band', 'Music', 2)
-    recommendObject.addClub('Students For Life', 'Political', 3)
+    recommendObject.addClub('A Xavier Christmas', 'Service', 3)
     return None
 
-def createUsers():
-    global u1
-    global u2
-    u1 = recommendObject.addUser(1)
-    u2 = recommendObject.addUser(2)
-    return None
-
-def addUserClubs():
-    u1.addClub('Pep Band', recommendObject)
-    u1.addClub('Computer Science Club', recommendObject)
-
-    u2.addClub('Pep Band', recommendObject)
-    u2.addClub('Students For Life', recommendObject)
-    u2.addClub('Computer Science Club', recommendObject)
+def addSmallClubs():
+    recommendObject.addClub('United for Uganda', 'Service', 4)
+    recommendObject.addClub('In the Loop', 'Sewing', 5)
+    recommendObject.addClub('Outdoor Club', 'Fun', 6)
+    recommendObject.addClub('4 Paws for Ability', 'Animals', 7)
+    recommendObject.addClub('Honors Council', 'Academic', 8)
     return None
 
 def checkForConnectionBetweenClubs():
     recommendObject.print_clubs()
     return None
 
-def main_1():
-    addClubs()
-    createUsers()
-    addUserClubs()
-    #checkForConnectionBetweenClubs()
+def miniDataSet():
+    global u1, u2
+    addMiniClubs()
+
+    u1 = recommendObject.addUser(1)
+    u2 = recommendObject.addUser(2)
+
+    u1.addClub('Pep Band', recommendObject)
+    u1.addClub('Computer Science Club', recommendObject)
+
+    u2.addClub('Pep Band', recommendObject)
+    u2.addClub('A Xavier Christmas', recommendObject)
+    u2.addClub('Computer Science Club', recommendObject)
+
+    miniRecommendations()
+    return None
+
+def miniRecommendations():
+    print("Mini Recommendations: ")
     a = recommendObject.createUserRecommendations(1)
     print("recommendation for user 1: " + a.destination.name + '\n')
 
     b = recommendObject.createUserRecommendations(2)
+    print("recommendation for user 2: " + b.destination.name + '\n')
+    return None
+
+def smallDataSet():
+    global u3, u4
+    addSmallClubs()
+
+    u3 = recommendObject.addUser(3)
+    u4 = recommendObject.addUser(4)
+
+    u3.addClub('A Xavier Christmas', recommendObject)
+    u3.addClub('4 Paws for Ability', recommendObject)
+
+    u4.addClub('A Xavier Christmas', recommendObject)
+
+    smallRecommendations()
+
+    return None
+
+
+def smallRecommendations():
+    print("Small Recommendations: ")
+    a = recommendObject.createUserRecommendations(3)
+    print("recommendation for user 3: " + a.destination.name + '\n')
+
+    b = recommendObject.createUserRecommendations(4)
     print("recommendation for user 2: " + b.destination.name)
     return None
 
-main_1()
+recommendObject = recommender.Recommender()
+miniDataSet()
+smallDataSet()
